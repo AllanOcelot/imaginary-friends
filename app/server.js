7f328a14edcda8a45393bda08c9cccfe8d4cc27e
Allan McKernan <meagusxlr@hotmail.com>
2018-08-02 18:22:25 +0100

    - We now have a working get command for teams on our local mongoDB

--- app/server.js
+++ app/server.js
@@ -1,67 +1,58 @@
-//Will house all of our simple CRUD examples, only for testing whilst
-//We build the TTOOL - www.github.com/allanocelot/tournament-tool
+var express = require('express');
+var lodash = require('lodash');
+var assert = require('assert')
+var mongoose = require('mongoose');
+var ObjectId = require('mongodb').ObjectId;
+var MongoClient = require('mongodb').MongoClient;
+
+const PORT = 3000;
+const server = express();
+
+//Variables here
+let db;
 
 
-// BASE SETUP
-// =============================================================================
-
-//Import API MOdels
-var Team = require('./models/team');
-
-
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/imaginaryfriends'); // connect to our database
-
-
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+//Mongoose Connection
+MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
+    // Client returned
+    db = client.db('imaginaryfriends');
+    console.log('connected');
+    //client.close();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+server.get('/', function(req, res){
+    res('')
 });
 
-
-router.route('/teams').post(function(req, res) {
-
-        var team = new Team();
-        team.name = req.body.name;
-
-        team.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Team created!' });
+//If user GET's the this route, we want to provide a team via ID if specified, if not, return all VISIBLE teams
+server.get('/api/teams/:id', function(req, res){
+    var collection = db.collection("teams");
+    //If user has requested a team via specific id
+    if(req.params.id){
+        var id = req.params.id;
+        var o_id = new ObjectId(id);
+        collection.find({"_id":o_id}).toArray(function(err, cursor) {
+            if(err){
+                console.log(err);
+            }else{
+                console.log(cursor);
+            }
         });
+        console.log('Client requested team via id ' + o_id + '.');
+    //If user has not included an id in their request
+    }else{
+        collection.find({"visible": true}).toArray(function(err, cursor) {
+            if(err){
+                console.log(err);
+            }else{
+                console.log(cursor);
+            }
+        });
+        console.log('Client did not request user via ID');
+    }
 });
 
-// more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Imaginary Friends, Booting up ' + port);
+server.listen(3000, function(){
+    console.log('Testing');
+});
